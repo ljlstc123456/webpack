9154689c5d13d5ddf48e6b9c2a9ee454a3312edc
lijialong@doctorwork.com <lijialong@doctorwork.com>
2018-04-10 18:20:28 +0800

    chapter6-1

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/index.js'
@@ -13,6 +14,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new UglifyJSPlugin(),
     new HtmlWebpackPlugin({
       title: 'Development'
     }),
